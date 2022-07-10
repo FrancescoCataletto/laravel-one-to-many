0fbb039936717f70bab75657f5660fb68e5f4fca
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-07-10 13:37:02 +0200

    printed in page the category of each post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -15,6 +15,7 @@
                 <td>ID</td>
                 <td>TITLE</td>
                 <td>TEXT</td>
+                <td>CATEGORY</td>
                 <td>AZIONI</td>
             </tr>
         </thead>
@@ -24,6 +25,7 @@
                 <td>{{$post->id}}</td>
                 <td>{{$post->title}}</td>
                 <td>{{$post->text}}</td>
+                <td>{{$post->category->name}}</td>
                 <td>
                     <a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-primary">SHOW</a>
                     <a href="{{route('admin.posts.edit', $post)}}" class="btn btn-success">EDIT</a>
